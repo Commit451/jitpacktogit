cbac2396d43e2030f916965b7ad0191e44970193
John Carlson <jawnnypoo@gmail.com>
2017-02-01 20:25:52 -0600

    Basic test to assure the plugin is being applied properly

--- jitpacktogit/src/test/java/com/commit451/jitpacktogit/IntegrationTest.kt
+++ jitpacktogit/src/test/java/com/commit451/jitpacktogit/IntegrationTest.kt
@@ -1,24 +1,19 @@
 package com.commit451.jitpacktogit
 
-import org.gradle.testkit.runner.GradleRunner
+import org.gradle.testfixtures.ProjectBuilder
+import org.junit.Assert
 import org.junit.Test
-import java.io.File
 
 /**
  * Test the integration
+ * @see <a href="https://docs.gradle.org/current/userguide/custom_plugins.html#sec:writing_tests_for_your_plugin">https://docs.gradle.org/current/userguide/custom_plugins.html#sec:writing_tests_for_your_plugin</a>
  */
 class IntegrationTest {
 
-    val integrationRoot = File("src/test/integration")
-
     @Test
-    fun integrationTests() {
-        System.out.println(integrationRoot.absolutePath)
-        val runner = GradleRunner.create()
-                .withProjectDir(integrationRoot)
-                .withPluginClasspath()
-                .withArguments("clean", "jitpacktogit", "--stacktrace")
-
-        val result = runner.build()
+    fun pluginFoundTest() {
+        val project = ProjectBuilder.builder().build()
+        project.pluginManager.apply("com.commit451.jitpacktogit")
+        Assert.assertTrue(project.tasks.getByName("jitpacktogit") is JitPackToGitTask)
     }
 }
